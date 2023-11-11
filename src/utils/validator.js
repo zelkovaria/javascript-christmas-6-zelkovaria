ebc52befa9eee1e589efed1bcf7b397c939cd640
zelkovaria <hyein396@gmail.com>
2023-11-11 11:40:43 +0900

    fix: 날짜 입력시 문자 포함 예외처리 추가

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -4,11 +4,16 @@ import MenuParser from "../utils/MenuParser.js";
 
 class Validator {
   static validateDate(date) {
+    const isOnlyNumbers = /^\d+$/.test(date);
+    if (!isOnlyNumbers) {
+      throw new Error(MESSAGE.INVALID_DATE);
+    }
+
     let inputDate = parseInt(date, 10);
-    const isNotANumber = isNaN(inputDate);
-    const isValid =
+    const isOutOfRange =
       inputDate > CONSTANTS.MAX_DATE || inputDate < CONSTANTS.MIN_DATE;
-    if (isValid || isNotANumber) {
+
+    if (isOutOfRange) {
       throw new Error(MESSAGE.INVALID_DATE);
     }
   }
