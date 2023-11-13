80ff3a7dc68ddfa64796acb8cf3803dba290c886
zelkovaria <hyein396@gmail.com>
2023-11-13 23:29:40 +0900

    test: validateDate 영어 입력시 예외처리 test 구현

--- __tests__/validatorTest.js
+++ __tests__/validatorTest.js
@@ -2,7 +2,11 @@ import Validator from "../src/utils/validator";
 import { MESSAGE } from "../src/constants/message";
 
 describe("ValidateTest", () => {
-  test("입력한 날짜는 문자가 들어가면 예외가 발생한다.", () => {
+  test("입력한 날짜는 특수문자가 들어가면 예외가 발생한다.", () => {
     expect(() => Validator.validateDate("3!@")).toThrow(MESSAGE.INVALID_DATE);
+  });
+
+  test("입력한 날짜는 영어가 들어가면 예외가 발생한다", () => {
+    expect(() => Validator.validateDate("3a")).toThrow(MESSAGE.INVALID_DATE);
   });
 });
