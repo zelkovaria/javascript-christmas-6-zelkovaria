4a8f5388047693919298d101f39a3151f52b45c1
zelkovaria <hyein396@gmail.com>
2023-11-14 17:31:45 +0900

    test: weekdayDiscount 할인 해당 경우 test 구현

--- __tests__/PromotionTest.js
+++ __tests__/PromotionTest.js
@@ -51,4 +51,15 @@ describe("PromotionTest", () => {
     const expectedDiscount = 3400;
     expect(promotion.dDayDiscount(date)).toBe(expectedDiscount);
   });
+
+  test("평일 할인은 1만원 이상 주문시 일~금요일 기준 디저트 메뉴1개당 2023원 할인한다", () => {
+    const promotion = new Promotion();
+    const date = 10;
+    const menus = [
+      { name: "해물파스타", quantity: 1 },
+      { name: "초코케이크", quantity: 2 },
+    ];
+    const expectedDiscount = 4046;
+    expect(promotion.weekdayDiscount(date, menus)).toBe(expectedDiscount);
+  });
 });
