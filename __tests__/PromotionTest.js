35c1fc6cf6adf2790a5901e66789c80b446a8b0b
zelkovaria <hyein396@gmail.com>
2023-11-14 19:51:55 +0900

    test: weekendDiscount 주말에 메인 요리 할인 test 구현

--- __tests__/PromotionTest.js
+++ __tests__/PromotionTest.js
@@ -84,4 +84,15 @@ describe("PromotionTest", () => {
     const expectedDiscount = 0;
     expect(promotion.weekdayDiscount(date, menus)).toBe(expectedDiscount);
   });
+
+  test("주말 할인은 1만원 이상 주문시 금~토요일 기준 메인 메뉴1개당 2023원 할인한다", () => {
+    const promotion = new Promotion();
+    const date = 15;
+    const menus = [
+      { name: "티본스테이크", quantity: 1 },
+      { name: "제로콜라", quantity: 1 },
+    ];
+    const expectedDiscount = 2023;
+    expect(promotion.weekendDiscount(date, menus)).toBe(expectedDiscount);
+  });
 });
