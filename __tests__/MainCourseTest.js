d5d4fbc476e2c76ab885b4348c9d33f3f846a212
zelkovaria <hyein396@gmail.com>
2023-11-14 13:12:51 +0900

    test: mainCourseProcessOrder 존재하는 메뉴에 대한 결과 반환 test 구현

---
+++ __tests__/MainCourseTest.js
@@ -0,0 +1,22 @@
+import { MAIN_COURSE_ITEMS } from "../src/constants/constants.js";
+import MainCourse from "../src/models/MainCourse.js";
+
+describe("MainCourseTest", () => {
+  let maincourse;
+
+  beforeEach(() => {
+    maincourse = new MainCourse();
+  });
+
+  test("주문된 메인코스는 존재하는 메뉴에 대해서만 결과를 반환한다", () => {
+    const menus = [
+      { name: MAIN_COURSE_ITEMS[0].name, quantity: 1 },
+      { name: "존재하지 않는 메뉴", quantity: 2 },
+    ];
+    const processedOrders = maincourse.mainCourseProcessOrder(menus);
+
+    expect(processedOrders).toEqual([
+      { name: MAIN_COURSE_ITEMS[0].name, quantity: 1 },
+    ]);
+  });
+});
