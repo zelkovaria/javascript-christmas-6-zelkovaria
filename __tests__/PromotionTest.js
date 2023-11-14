3db2dec432c2c6ad93baa286bfce2c8439e2e314
zelkovaria <hyein396@gmail.com>
2023-11-14 21:17:36 +0900

    refactor: 불필요한 인스턴스 생성 삭제

--- __tests__/PromotionTest.js
+++ __tests__/PromotionTest.js
@@ -9,7 +9,6 @@ describe("PromotionTest", () => {
   });
 
   test("주어진 메뉴의 총 가격을 할인 전 가격으로 계산한다", () => {
-    const promotion = new Promotion();
     const menus = [
       { name: "티본스테이크", quantity: 2 },
       { name: "레드와인", quantity: 1 },
@@ -39,21 +38,18 @@ describe("PromotionTest", () => {
   });
 
   test("d-Day할인은 12/1~12/25사이 기간동안 1000원부터 100원씩 증가된다", () => {
-    const promotion = new Promotion();
     const date = 16;
     const expectedDiscount = 2500;
     expect(promotion.dDayDiscount(date)).toBe(expectedDiscount);
   });
 
   test("d-Day할인은 12/25이후 최대 할인 금액이 3400원이다", () => {
-    const promotion = new Promotion();
     const date = 27;
     const expectedDiscount = 3400;
     expect(promotion.dDayDiscount(date)).toBe(expectedDiscount);
   });
 
   test("평일 할인은 1만원 이상 주문시 일~금요일 기준 디저트 메뉴1개당 2023원 할인한다", () => {
-    const promotion = new Promotion();
     const date = 10;
     const menus = [
       { name: "해물파스타", quantity: 1 },
@@ -64,7 +60,6 @@ describe("PromotionTest", () => {
   });
 
   test("평일 할인은 1만원 이상 주문시 일~금요일 기준 디저트 메뉴가 없으면 할인이 없다", () => {
-    const promotion = new Promotion();
     const date = 10;
     const menus = [
       { name: "해물파스타", quantity: 1 },
@@ -75,7 +70,6 @@ describe("PromotionTest", () => {
   });
 
   test("평일 할인은 주말엔 할인하지 않는다", () => {
-    const promotion = new Promotion();
     const date = 29;
     const menus = [
       { name: "해물파스타", quantity: 1 },
@@ -86,7 +80,6 @@ describe("PromotionTest", () => {
   });
 
   test("주말 할인은 1만원 이상 주문시 금~토요일 기준 메인 메뉴1개당 2023원 할인한다", () => {
-    const promotion = new Promotion();
     const date = 15;
     const menus = [
       { name: "티본스테이크", quantity: 1 },
@@ -97,7 +90,6 @@ describe("PromotionTest", () => {
   });
 
   test("주말 할인은 1만원 이상 주문시 금~토요일 기준 메인 메뉴가 없으면 할인이 없다", () => {
-    const promotion = new Promotion();
     const date = 15;
     const menus = [
       { name: "초코케이크", quantity: 3 },
@@ -108,7 +100,6 @@ describe("PromotionTest", () => {
   });
 
   test("주말 할인은 평일엔 할인하지 않는다", () => {
-    const promotion = new Promotion();
     const date = 25;
     const menus = [
       { name: "티본스테이크", quantity: 1 },
@@ -120,7 +111,6 @@ describe("PromotionTest", () => {
   });
 
   test("샴페인 증정 상황시 샴페인 가격을 반환한다", () => {
-    const promotion = new Promotion();
     const menus = [
       { name: "티본스테이크", quantity: 2 },
       { name: "해산물파스타", quantity: 1 },
@@ -130,7 +120,6 @@ describe("PromotionTest", () => {
   });
 
   test("샴페인 미증정 상황시 0원을 반환한다", () => {
-    const promotion = new Promotion();
     const menus = [
       { name: "해산물파스타", quantity: 1 },
       { name: "제로콜라", quantity: 1 },
@@ -139,26 +128,22 @@ describe("PromotionTest", () => {
   });
 
   test("달력에 별이 있는 날짜에는 특별할인 1000원이 추가된다", () => {
-    const promotion = new Promotion();
     const date = 25;
     expect(promotion.specailDiscount(date)).toBe(1000);
   });
 
   test("달력에 별이 없는 날짜에는 특별할인이 없다", () => {
-    const promotion = new Promotion();
     const date = 29;
     expect(promotion.specailDiscount(date)).toBe(0);
   });
 
   test("할인전 금액이 1만원 미만인 경우 할인혜택이 없다", () => {
-    const promotion = new Promotion();
     const date = 24;
     const menus = [{ name: "양송이수프", quantity: 1 }];
     expect(promotion.calculateTotalDiscount(date, menus)).toBe(0);
   });
 
   test("할인전 금액이 1만원 이상인 경우 할인혜택을 제공한다", () => {
-    const promotion = new Promotion();
     const date = 3;
     const menus = [
       { name: "티본스테이크", quantity: 1 },
